a4545a93a7870b25deb18897d452d2905756d6c7
Mansour Mahboubi <mansourmahboubi@protonmail.com>
2022-10-29 15:29:23 +0200

    feat: add django-ninja support

---
+++ apps/accounts/api/views.py
@@ -0,0 +1,6 @@
+from ninja import Router
+router = Router()
+
+@router.get('')
+def health_check(request):
+    return {"status": "up"}
---
+++ core/api.py
@@ -0,0 +1,6 @@
+from ninja import NinjaAPI
+from apps.accounts.api.views import router as users_router
+
+api = NinjaAPI()
+
+api.add_router("/status/", users_router)
