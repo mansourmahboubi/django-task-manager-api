b6a8a3cb65426da196ae2bc12f336b8f7d83fe9c
Mansour Mahboubi <mansourmahboubi@protonmail.com>
2022-10-29 16:32:15 +0200

    feat: add mypy

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from typing import Optional, List
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = "django-insecure-(mw8*ps^m$*c(z$1$(a@35^m#x!sobthb661tq(^u^jv^3jq)h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: List[Optional[str]] = []
 
 
 # Application definition
