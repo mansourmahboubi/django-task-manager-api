714c15a7da6259579d076722bbeae348311e18e1
Mansour Mahboubi <mansourmahboubi@protonmail.com>
2022-11-04 09:10:35 +0100

    fix: refactor unit tests

--- apps/task_manager/tasks/tests.py
+++ apps/task_manager/tasks/tests.py
@@ -1,4 +1,5 @@
 import pytest
+from django.contrib.auth.models import Group
 from ninja.errors import HttpError
 
 from apps.accounts.models import User
@@ -40,17 +41,20 @@ class TestSetAssignTask:
     def test_sets_un_authorized_assign(self, mocker, task, user, assigner):
 
         mock_create = mocker.patch.object(models.TaskAssignee.objects, "create")
+        mock_has_perm = mocker.patch.object(user, "has_perm", return_value=False)
 
         with pytest.raises(HttpError) as error:
             # call with regular user
             services.assign_task(task=task, user=user, assigner=user)
 
+        mock_has_perm.assert_called_once_with("tasks.add_taskassignee")
         mock_create.assert_not_called()
 
     def test_sets_authorized_assign(self, mocker, task, user, assigner):
-
         mock_create = mocker.patch.object(models.TaskAssignee.objects, "create")
+        mock_has_perm = mocker.patch.object(assigner, "has_perm", return_value=True)
 
         services.assign_task(task=task, user=user, assigner=assigner)
 
+        mock_has_perm.assert_called_once_with("tasks.add_taskassignee")
         mock_create.assert_called_once_with(task=task, user=user)
